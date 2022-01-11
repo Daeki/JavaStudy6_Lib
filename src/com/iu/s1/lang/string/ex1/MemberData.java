ddab5a4a7e992b08ca1a33171a00733c5cf83947
Daeki <kimdaeki@gmail.com>
2022-01-11 16:16:01 +0900

    1

--- src/com/iu/s1/lang/string/ex1/MemberData.java
+++ src/com/iu/s1/lang/string/ex1/MemberData.java
@@ -6,17 +6,17 @@ public class MemberData {
 	
 	//기본 생성자 선언
 	public MemberData() {
-		this.data="id1-pw1-name1-id1@gmail.com-";
-		this.data=this.data+"id2 - pw2 - name2 - name2@naver.com-";
-		this.data=this.data+"id3 - pw3 - name3 - name3@daum.net-";
-		this.data=this.data+"id4 - pw4 - name4- name4@daum.net-";
+		this.data="id1-pw1-name1-id1@gmail.com-20-";
+		this.data=this.data+"id2 - pw2 - name2 - name2@naver.com-42-";
+		this.data=this.data+"id3 - pw3 - name3 - name3@daum.net-36-";
+		this.data=this.data+"id4 - pw4 - name4- name4@daum.net-17-";
 		System.out.println(this.data);
 	}
 	
 	public MemberDTO [] init() {
 		String [] datas = this.data.split("-");
 		
-		MemberDTO [] members = new MemberDTO[datas.length/4];
+		MemberDTO [] members = new MemberDTO[datas.length/5];
 		
 		int index=0;
 
@@ -28,6 +28,7 @@ public class MemberData {
 			memberDTO.setPw(datas[index++]);    //datas[1] 5  9
 			memberDTO.setName(datas[index++]);  //datas[2] 6  10
 			memberDTO.setEmail(datas[index++]); //datas[3] 7  11
+			memberDTO.setAge(Integer.parseInt(datas[index++]));
 			members[i]=memberDTO;
 			//index++;
 		}
