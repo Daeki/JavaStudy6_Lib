ce83e0bedd705bd67cbfb95c80f2c9fb02b29b6d
Daeki <kimdaeki@gmail.com>
2022-01-13 15:28:20 +0900

    1

--- src/com/iu/s3/ex1/Ex1Main.java
+++ src/com/iu/s3/ex1/Ex1Main.java
@@ -2,7 +2,9 @@ package com.iu.s3.ex1;
 
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import com.iu.s3.ex1.member.MemberDTO;
 import com.iu.s3.ex1.member.MemberData;
@@ -19,6 +21,25 @@ public class Ex1Main {
 		//회원들의 정보 모음
 		ArrayList<MemberDTO> ar = memberData.init();
 		Map<String, StudentDTO> map = studentData.addStudent(ar);
+		Set<String> keys = map.keySet();
+		Iterator<String> k = keys.iterator();
+		
+		while(k.hasNext()) {
+			String key = k.next();
+			StudentDTO studentDTO = map.get(key);
+			System.out.println("Key : "+ key);
+			System.out.println(studentDTO.getId());
+			System.out.println(studentDTO.getName());
+			System.out.println(studentDTO.getKor());
+			System.out.println(studentDTO.getEng());
+			System.out.println(studentDTO.getMath());
+			System.out.println(studentDTO.getTotal());
+			System.out.println(studentDTO.getAvg());
+			
+		}
+		
+		
+		
 		System.out.println(map);
 
 	}
