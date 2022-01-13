908f10a24a65f386f6ea9ca1923f5c6a6e828303
Daeki <kimdaeki@gmail.com>
2022-01-13 15:11:18 +0900

    1

--- src/com/iu/s2/util/collection/CollectionMain.java
+++ src/com/iu/s2/util/collection/CollectionMain.java
@@ -1,10 +1,20 @@
 package com.iu.s2.util.collection;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class CollectionMain {
 
 	public static void main(String[] args) {
-		ListStudy ls = new ListStudy();
-		ls.study3();
+		//ListStudy ls = new ListStudy();
+		//List<String> ar= ls.study5();
+		
+		//new SetStudy().study4();
+		
+		new MapStudy().study1();
+		
+		
 	}
 
 }
