fe97f8620fc0a1c493f9eac5c4cf72539a3b5d87
Daeki <kimdaeki@gmail.com>
2022-01-17 17:53:40 +0900

    tt

---
+++ src/com/iu/s4/exception/ExceptionMain2.java
@@ -0,0 +1,25 @@
+package com.iu.s4.exception;
+
+public class ExceptionMain2 {
+
+	public static void main(String[] args) {
+		
+		int num1=0;
+		int num2=10;
+		int result=0;
+		try {
+			result = num2/num1;
+		}catch(Exception exception) {
+			
+			exception.printStackTrace();
+			System.out.println("예외 발생");
+				
+		}
+		
+		//result = num2/num1;
+		System.out.println(result);
+		
+
+	}
+
+}
---
+++ src/com/iu/s5/ex1/CilentMain.java
@@ -0,0 +1,55 @@
+package com.iu.s5.ex1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class CilentMain {
+
+	public static void main(String[] args) {
+		System.out.println("Client 입니다");
+		Socket socket=null;
+		Scanner scanner = new Scanner(System.in);
+		try {
+			socket = new Socket("192.168.10.245", 8282);
+			System.out.println("서버와 접속이 성공!!!");
+			System.out.println("서버로 보낼 메세지 입력");
+			String message= scanner.nextLine();
+			System.out.println(message);//hello -> h, e, l, l o -> 01
+			
+			//1. output Stream을 구해오기
+			 OutputStream os = socket.getOutputStream();//byte(bit처리)
+			 OutputStreamWriter ow = new OutputStreamWriter(os);//char(문자 처리)
+			 BufferedWriter bw = new BufferedWriter(ow);
+			 //전송 준비 끝
+			 
+			 //전송
+			 bw.write(message+"\r\n");
+			 //강제로 버퍼 비우기
+			 bw.flush();
+			 
+			 InputStream is = socket.getInputStream();
+			 InputStreamReader ir = new InputStreamReader(is);
+			 BufferedReader br = new BufferedReader(ir);
+			 
+			 message = br.readLine();
+			 
+			 System.out.println("Server : "+message);
+			 
+			 
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+
+	}
+
+}
